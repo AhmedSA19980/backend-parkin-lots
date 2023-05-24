6392601c3b3da55460b09355724b73fb74316c15
miche <AhmedSA19980>
2023-05-24 09:27:50 +0300

    database

---
+++ prisma/migrations/20230520082634_new_add/migration.sql
@@ -0,0 +1,5 @@
+-- RenameIndex
+ALTER TABLE `_parkinglottovehicletype` RENAME INDEX `_ParkingLotToVehicleType_AB_unique` TO `_parkinglottovehicletype_AB_unique`;
+
+-- RenameIndex
+ALTER TABLE `_parkinglottovehicletype` RENAME INDEX `_ParkingLotToVehicleType_B_index` TO `_parkinglottovehicletype_B_index`;
