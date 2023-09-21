4e9563beaf41c31b46f75c78aca4057d6d1321ea
miche <AhmedSA19980>
2023-09-21 14:41:06 +0300

    query users and bycrypt pass

--- back-end/user/users.ts
+++ back-end/user/users.ts
@@ -6,27 +6,18 @@ import express, { Application, Request, Response } from "express";
 
 export default function(app:Application){
     app.get("/users" , async(req:Request, res:Response)=>{
-        const query_all_users  =  await prisma.user.findMany({
-            select:{
-               // password:false,
-                name:true,
-                email:true,
-                isuserdeleted:true,
-            }
-        })
+        const query_all_users = await prisma.user.findMany({
+          select: {
+            //password:true, //pass is hashed
+            id: true,
+            name: true,
+            email: true,
+            isuserdeleted: true,
+          },
+        });
         
         return res.status(200).json({users:query_all_users})
 
     })
 }
 
-
-async function excludePasswordMiddleware(params:any, next:any) {
-  const result = await next(params);
-  if (params?.model === "User" && params?.args?.select?.password !== true) {
-    delete result.password;
-  }
-  return result;
-}
-
-prisma.$use(excludePasswordMiddleware);
