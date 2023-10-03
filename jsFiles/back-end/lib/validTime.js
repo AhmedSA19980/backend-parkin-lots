25c08e2b1f7e0c98e2ee6cd93fbc2e8dcb2dd8a2
Alpha Guy <61384071+AhmedSA19980@users.noreply.github.com>
2023-10-03 23:59:35 +0300

    Delete jsFiles directory

--- jsFiles/back-end/lib/validTime.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidTime = void 0;
-function isValidTime(time) {
-    const currentTime = new Date();
-    const inputTime = new Date(time);
-    return inputTime > currentTime;
-}
-exports.isValidTime = isValidTime;
