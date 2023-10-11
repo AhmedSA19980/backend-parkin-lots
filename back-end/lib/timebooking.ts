833a0c9fcc24787af045e769f79113a279f19d5e
Alpha Guy <61384071+AhmedSA19980@users.noreply.github.com>
2023-10-11 16:52:03 +0300

    Master (#25)
    * add user api
    
    * allow user to update
    
    * database
    
    * allow user to update1
    
    * database
    
    * add basic book feature
    
    * view book info
    
    * add payment intent
    
    * query all users
    
    * delete user
    
    * back-end intruction
    
    * read
    
    * modify del route
    
    * add CREATING LOTS feature
    
    * allow specific park to be updated
    
    * encrypt user.password
    
    * modify del
    
    * query user data
    
    * book
    
    * modify park and run validation
    
    * delete
    
    * update
    
    * query users and bycrypt pass
    
    * rules
    
    * database
    
    * resolved confilcts book.ts
    
    * user
    
    * resolve confict
    
    * lot  validation
    
    * validate query book
    
    * removed file querybook
    
    * Delete jsFiles directory
    
    * Delete node_modules directory
    
    * Delete OOP.py
    
    * Delete test/user directory
    
    * Delete back-end/lib/pay.ts
    
    * Delete back-end/lib/localtime.ts
    
    * Delete back-end/lib/timebooking.ts
    
    * Delete .env
    
    * Delete back-end/calc directory
    
    * Delete back-end/config directory
    
    * Delete back-end/payment directory
    
    * Delete back-end/booking.ts
    
    * Delete back-end/parkinglots.ts
    
    * Delete back-end/readuser.ts
    
    * book
    
    * book
    
    * book
    
    * book
    
    * clean index file
    
    * REname file
    
    * Master (#18) (#19)
    
    * add user api
    
    * allow user to update
    
    * database
    
    * allow user to update1
    
    * database
    
    * add basic book feature
    
    * view book info
    
    * add payment intent
    
    * query all users
    
    * delete user
    
    * back-end intruction
    
    * read
    
    * modify del route
    
    * add CREATING LOTS feature
    
    * allow specific park to be updated
    
    * encrypt user.password
    
    * modify del
    
    * query user data
    
    * book
    
    * modify park and run validation
    
    * delete
    
    * update
    
    * query users and bycrypt pass
    
    * rules
    
    * database
    
    * resolved confilcts book.ts
    
    * user
    
    * resolve confict
    
    * lot  validation
    
    * validate query book
    
    * removed file querybook
    
    * Delete jsFiles directory
    
    * Delete node_modules directory
    
    * Delete OOP.py
    
    * Delete test/user directory
    
    * Delete back-end/lib/pay.ts
    
    * Delete back-end/lib/localtime.ts
    
    * Delete back-end/lib/timebooking.ts
    
    * Delete .env
    
    * Delete back-end/calc directory
    
    * Delete back-end/config directory
    
    * Delete back-end/payment directory
    
    * Delete back-end/booking.ts
    
    * Delete back-end/parkinglots.ts
    
    * Delete back-end/readuser.ts
    
    * book
    
    * book
    
    * book
    
    * book
    
    * clean index file
    
    * readmd
    
    * clean
    
    * pack
    
    * packlock
    
    * update
    
    * update
    
    
    ---------
    
    * lib
    
    ---------
    
    Co-authored-by: miche <AhmedSA19980>

---
+++ back-end/lib/timebooking.ts
@@ -0,0 +1,72 @@
+import { PrismaClient } from "@prisma/client";
+import { localTime } from "./localtime";
+import { id } from "date-fns/locale";
+const prisma = new PrismaClient();
+
+
+
+export async function flashoutExpiredBookingTime() {
+  // Find all bookings that have ended
+
+  const compeletedBookings = await prisma.booking.findMany({
+    where: {
+      endTime: {
+        lt: new Date(),
+      },
+    },
+    include: { parkinglot: true },
+  });
+
+  // Update the capacity of the associated parking lots
+  for (const booking of compeletedBookings) {
+    const time = localTime(booking.endTime);
+
+    if (
+      booking &&
+      new Date() > booking.endTime &&
+      !booking.capacityUsedDecremented
+    ) {
+      // Decrement capacityUsed by 1
+      await prisma.parkinglot.update({
+        where: { id: booking.parkinglot.id },
+        data: { capacityUsed: { decrement: 1 } },
+      });
+
+      // Set capacityUsedDecremented to true
+      await prisma.booking.update({
+        where: { id: booking.id },
+        data: { capacityUsedDecremented: true, isBookedCompeleted: true },
+      });
+    }
+  }
+}
+
+     
+/*if (
+      time &&
+      booking.parkinglot.capacityUsed > -1 &&
+      booking.parkinglot.capacityUsed <= booking.parkinglot.capacity
+    ){
+        const t =await  prisma.parkinglot.update({
+           where: { id: booking.parkinglot.id },
+           data: {
+             capacityUsed: { decrement: 1 },
+             booking: {
+               update: {
+                 where: { id: booking.parkingLotId },
+                 data: {
+                   capacityUsedDecremented: true,
+                   isBookedCompeleted: true,
+                 },
+               },
+             },
+           },
+         );
+           t
+         if (!booking.capacityUsedDecremented &&  t) {
+           await prisma.booking.update({
+             where: { id: booking.parkingLotId },
+             data: { capacityUsedDecremented: true, isBookedCompeleted: true },
+           });
+         }*/
+/**/ 
