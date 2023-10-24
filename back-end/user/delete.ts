cd69211a26cbe909e8974794430403aea29e7f6f
Alpha Guy <61384071+AhmedSA19980@users.noreply.github.com>
2023-10-24 17:44:35 +0300

    Master (#28)
    * add user api
    
    * allow user to update
    
    * database
    
    * allow user to update1
    
    * database
    
    * add basic book feature
    
    * view book info
    
    * add payment intent
    
    * query all users
    
    * delete user
    
    * back-end intruction
    
    * read
    
    * modify del route
    
    * add CREATING LOTS feature
    
    * allow specific park to be updated
    
    * encrypt user.password
    
    * modify del
    
    * query user data
    
    * book
    
    * modify park and run validation
    
    * delete
    
    * update
    
    * query users and bycrypt pass
    
    * rules
    
    * database
    
    * resolved confilcts book.ts
    
    * user
    
    * resolve confict
    
    * lot  validation
    
    * validate query book
    
    * removed file querybook
    
    * Delete jsFiles directory
    
    * Delete node_modules directory
    
    * Delete OOP.py
    
    * Delete test/user directory
    
    * Delete back-end/lib/pay.ts
    
    * Delete back-end/lib/localtime.ts
    
    * Delete back-end/lib/timebooking.ts
    
    * Delete .env
    
    * Delete back-end/calc directory
    
    * Delete back-end/config directory
    
    * Delete back-end/payment directory
    
    * Delete back-end/booking.ts
    
    * Delete back-end/parkinglots.ts
    
    * Delete back-end/readuser.ts
    
    * book
    
    * book
    
    * book
    
    * book
    
    * clean index file
    
    * REname file
    
    * Master (#18) (#19)
    
    * add user api
    
    * allow user to update
    
    * database
    
    * allow user to update1
    
    * database
    
    * add basic book feature
    
    * view book info
    
    * add payment intent
    
    * query all users
    
    * delete user
    
    * back-end intruction
    
    * read
    
    * modify del route
    
    * add CREATING LOTS feature
    
    * allow specific park to be updated
    
    * encrypt user.password
    
    * modify del
    
    * query user data
    
    * book
    
    * modify park and run validation
    
    * delete
    
    * update
    
    * query users and bycrypt pass
    
    * rules
    
    * database
    
    * resolved confilcts book.ts
    
    * user
    
    * resolve confict
    
    * lot  validation
    
    * validate query book
    
    * removed file querybook
    
    * Delete jsFiles directory
    
    * Delete node_modules directory
    
    * Delete OOP.py
    
    * Delete test/user directory
    
    * Delete back-end/lib/pay.ts
    
    * Delete back-end/lib/localtime.ts
    
    * Delete back-end/lib/timebooking.ts
    
    * Delete .env
    
    * Delete back-end/calc directory
    
    * Delete back-end/config directory
    
    * Delete back-end/payment directory
    
    * Delete back-end/booking.ts
    
    * Delete back-end/parkinglots.ts
    
    * Delete back-end/readuser.ts
    
    * book
    
    * book
    
    * book
    
    * book
    
    * clean index file
    
    ---------
    
    
    
    * update
    
    ---------
    
    Co-authored-by: miche <AhmedSA19980>

--- back-end/user/delete.ts
+++ back-end/user/delete.ts
@@ -5,27 +5,32 @@ import express, { Application, Request, Response } from "express";
 const prisma = new PrismaClient();
 
 type User = {
-  userId: number
+ 
   email:string
 }
 
 
 export default function(app:Application ,args?:any){
   app.post("/delete_user", async (req: Request, res: Response) => {
-    const { userId, email }: User = req.body;
+    const {  email }: User = req.body;
 
-    // Delete the user with the specified ID
-    const deletedUser = await prisma.user.update({
-      where: {
-        id: userId,
-        email:email
-      },
-      data: { isuserdeleted: true }, // this field doesn't show up
-    });
-    if (!deletedUser) {
-      res.status(400).json({ message: "user not found" });
-    } else {
+
+    const findUser  = await prisma.user.findUnique({
+      where:{email:email }
+    })
+    if (findUser?.email) {
+      // Delete the user with the specified ID
+      const deletedUser = await prisma.user.update({
+        where: {
+
+          email: email,
+        },
+        data: { isuserdeleted: true }, // this field doesn't show up
+      });
       return res.status(200).json({ isUserDeleted: deletedUser });
+    } else {
+        res.status(400).json({ message: "user not found" });       
+
     }
   });
 }  
